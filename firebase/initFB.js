79ab5cf505280065f9ca81ec6dc6db896b01e787
WasixXD <66091116+WasixXD@users.noreply.github.com>
2021-08-12 23:05:00 -0300

    Update initFB.js

--- firebase/initFB.js
+++ firebase/initFB.js
@@ -14,25 +14,6 @@ admin.initializeApp({
 const db = admin.firestore()
 const storage = admin.storage().bucket("gs://occasum-85395.appspot.com/")
 
-// fazer o arquivo publico e pegar o mediaUrl para poder mostrar
-// storage.makePublic({includeFiles: true}, (err, files) => {
-//     for(let file of files) {
-//         console.log(file.metadata)
-//     }
-// })
-
-// const file = "./uploads/piano"
-
-//fazer upload nele
-// storage.upload(file, {metadata: "image/jpg"}).then(( result ) => {
-//     console.log("foi", result)
-// })
-
-// storage.getFiles((err, files) => {
-//     for(let file of files) {
-//         console.log(file.metadata)
-//     }
-// })
 
 
 
