db766a2ccdd4a327c754b795e92fe3382bcdfd24
WasixXD <66091116+WasixXD@users.noreply.github.com>
2023-01-26 18:01:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3000
 const PATH = __dirname + "/occasum/public/"
 
-app.use(cors({origin: "*"}))
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'http://not-occasum.onrender.com', 'https://not-occasum.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 app.use(express.static(__dirname + "/occasum/public/"))
 app.use(express.urlencoded({extended: true}))
 
